53027b94d5e282a9700ab2b66db9a21e632ccdc6
Karina P <karina_pineda97@hotmail.com>
2024-10-18 13:49:24 -0500

    feat profile

--- src/app/(sections)/about/page.tsx
+++ src/app/(sections)/about/page.tsx
@@ -6,21 +6,21 @@ import { SiDart, SiFlutter, SiTypescript } from "react-icons/si";
 
 export default function AboutPage() {
     return (
-        <div className="flex flex-col md:flex-row bg-greenaqua text-gray-800 p-8">
-            <article className="w-full md:w-2/3 pr-8">
-                <h1 className="text-4xl md:text-5xl font-bold text-whites uppercase mb-4">
-                    hola! soy karina,<br />
+        <div className="flex flex-col-reverse justify-center align-center px-12 py-4 bg-greenaqua text-gray-800  md:flex-row">
+            <article className="w-full md:w-2/3">
+                <h1 className="text-lg font-bold text-whites uppercase mb-4 md:text-5xl">
+                    holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaq! soy karina,<br />
                     una <span className="text-salmon">Frontend developer</span><br />
-                    e ingeniera de sistemas <FaLaptopCode className="inline-block text-salmon ml-2" />
+                    e ingeniera de sistemas <FaLaptopCode className=" text-2xl text-salmon inline-block " />
                 </h1>
-                <div className="mb-6 text-whites">
+                <div className="mb-6  text-whites text-sm md:text-2xl">
                     <p className="mb-4">
-                        Vengo de una familia cafetera, con valores como la perseverancia, la empatía y el trabajo en equipo. Me gusta conectar con las personas,
-                        construir relaciones de confianza y aportar positivamente. Tengo la convicción de que la amabilidad y el respeto son esenciales para lograr
+                        Vengo de una familia cafetera y <span className="text-salmon capitalize">santandereana</span>, con valores como la perseverancia, la empatía y el trabajo en equipo. Me gusta conectar con las personas,
+                        construir relaciones de confianza y aportar positivamente. <br/>Tengo la convicción de que la amabilidad y el respeto son esenciales para lograr
                         cualquier objetivo. A nivel técnico trabajo en soluciones eficientes para una agradable experiencia de usuario.
                         Cuento con experiencia en el desarrollo de sitios web y aplicaciones móviles con JavaScript, Next.js y Flutter.
                     </p>
-                    <p className="text-lg mb-4  gap-2 flex flex-row">
+                    <p className=" text-4xl gap-6 text-salmon  flex-wrap mb-4  flex justify-around">
                         <FaReact />
                         <RiNextjsFill />
                         <SiFlutter />
@@ -33,7 +33,7 @@ export default function AboutPage() {
                     </p>
                 </div>
             </article>
-            <article className="w-full md:w-1/3 mt-8 md:mt-0">
+            <article className=" md:w-1/3 md:mt-0">
                 <div className="relative w-64 h-64 mx-auto">
                     <Image
                         src="/img/ka.jpg"
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,14 @@ import HeaderNavigate from './Nav-header';
 
 export default function Header() {
     return (
-        <header className="bg-greenforest h-50 text-whites justify-between items-center  py-2">
-            <section className="flex justify-center items-center py-4">
-                <Link href="/about" ><Image src="/img/logo1.png" alt="logo-coffee" width={50} height={60} /></Link>
-            </section>  
+        <header className="bg-greenforest text-whites flex flex-col gap-2 justify-center items-center py-2 md:flex-row md:justify-around ">
+            <Link href="/about" ><Image src="/img/logo1.png" alt="logo-coffee" width={50} height={60} /></Link>
             <HeaderNavigate />
-            <section className="flex justify-center items-center py-4  ">
-                <button className=" bg-salmon  text-whites px-6 py-4 rounded-full font-bold hover:bg-greenaqua transition-colors">
-                    Contáctame
-                    <Link href="/contact" ></Link>
-                </button>
-            </section>
+            <button className=" bg-salmon  text-whites px-4 py-1 rounded-full font-bold hover:bg-greenaqua transition-colors">
+                Contáctame
+                <Link href="/contact" ></Link>
+            </button>
+
         </header>
     )
 }
